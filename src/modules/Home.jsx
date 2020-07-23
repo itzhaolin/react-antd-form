9ec47547e52a2bb38481c981d624f191c73789b9
zhaolin <zhaolin@zhaolindeMacBook-Pro.local>
2020-07-23 10:00:53 +0800

    style

--- src/modules/Home.jsx
+++ src/modules/Home.jsx
@@ -8,7 +8,7 @@ function Home (props) {
     const { history } = props
     return(
         <div className={styles.home}>
-            <div>Home</div>
+            <div className="title">Home</div>
             <Button onClick={()=>history.push(`/newForm`)}>newForm</Button>
             <Button onClick={()=>history.push(`/classForm`)}>classForm</Button>
             <Button onClick={()=>history.push(`/hookForm`)}>hookForm</Button>
